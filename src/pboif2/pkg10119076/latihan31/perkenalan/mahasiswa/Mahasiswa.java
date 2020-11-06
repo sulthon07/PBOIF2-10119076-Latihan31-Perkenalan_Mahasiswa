4ae1e50c909d08e47f830f4288c140d413c32327
ACER <ACER@DESKTOP-JMQ2UKH>
2020-11-06 20:48:34 +0700

    PBOIF2-10119076-Latihan31-Perkenalan_Mahasiswa

---
+++ src/pboif2/pkg10119076/latihan31/perkenalan/mahasiswa/Mahasiswa.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119076.latihan31.perkenalan.mahasiswa;
+
+/**
+ *
+ * @author ACER
+ * Nama : Sulthon Naufal Akmal
+ * Kelas : IF2
+ * NIM : 10119076
+ * Deskripsi : Perkenaalan Mahasiswa
+ */
+public class Mahasiswa {
+    String nim;
+    String nama;
+       
+    public void PerkenalkanDiri() {
+    System.out.println("Hello Everyonne");
+    System.out.println("My NIM is " + nim);
+    System.out.println("My NAME is " + nama);
+    System.out.println();
+    
+         
+
+      
+ }
+
+    
+
+}
+
---
+++ src/pboif2/pkg10119076/latihan31/perkenalan/mahasiswa/PBOIF210119076Latihan31PerkenalanMahasiswa.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119076.latihan31.perkenalan.mahasiswa;
+
+/**
+ *
+ * @author ACER
+ * Nama : Sulthon Naufal Akmal
+ * Kelas : IF2
+ * NIM : 10119076
+ * Deskripsi : Perkenaalan Mahasiswa
+ */
+public class PBOIF210119076Latihan31PerkenalanMahasiswa {
+
+
+    /**
+     * @param args the command line arguments
+     */
+
+    
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa();
+
+        
+
+        mhs.nim = "10119076";
+
+        mhs.nama = "Sulthon Naufal Akmal";
+
+        
+
+        mhs.PerkenalkanDiri();
+
+        
+
+        mhs.nim = "10119077";
+
+        mhs.nama = "Ridwan Anwar Fauzan";
+
+        
+
+        mhs.PerkenalkanDiri();
+
+        
+
+        mhs.nim = "10119078";
+
+        mhs.nama = "Adam Firdaus";
+
+        
+
+        mhs.PerkenalkanDiri();
+
+        
+
+        mhs.nim = "10119079";
+
+        mhs.nama = "Rafi Khalifanan";
+
+        
+
+        mhs.PerkenalkanDiri();
+
+        
+
+
+
+    }
+
+    
+
+}
+    
+    
+
+
